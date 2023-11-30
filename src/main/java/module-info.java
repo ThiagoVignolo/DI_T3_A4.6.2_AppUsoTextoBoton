a1de36e597deec86e23705b10aaa730ba1dc6228
Thiago Vignolo Ferreira <thiagovignoloferreira@gmail.com>
2023-11-30 13:05:41 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module es.ieslosmontecillos.di_t3a462_appusotextoboton {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens es.ieslosmontecillos.di_t3a462_appusotextoboton to javafx.fxml;
+    exports es.ieslosmontecillos.di_t3a462_appusotextoboton;
+}
